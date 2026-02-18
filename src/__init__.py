d677a63f68c2adba23eae3fc367e87364d8c56c0
nightmorsky <nightmorsky@gmail.com>
2026-02-18 19:31:55 +0200

    Refactor PDF module: extract splitter and merger to separate files

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,49 @@
 """
-XLSX to XML Converter Package
+Rowline Converter Package
+
+Modules:
+    - xlsx2xml: Convert XLSX files to XML format
+    - pdf: PDF conversion, splitting, and merging
+    - gif: Convert video files to GIF
+    - api: FastAPI web server
+    - cli: Command-line interface
 """
 
 from .xlsx2xml import XlsxToXmlConverter, XlsxReader, XmlWriter, XmlFiller
 from .pdf import (
-    any_to_pdf, any_to_pdf_async, UnsupportedFormat, ConversionProgress,
-    split_pdf, merge_pdf
+    # Conversion
+    any_to_pdf,
+    any_to_pdf_async,
+    UnsupportedFormat,
+    ConversionProgress,
+    # Splitter
+    split_pdf,
+    split_pdf_sequential,
+    split_pdf_parallel,
+    # Merger
+    merge_pdf,
+    merge_pdf_sequential,
+    merge_pdf_parallel,
 )
 
 __version__ = "1.0.0"
 __all__ = [
-    "XlsxToXmlConverter", "XlsxReader", "XmlWriter", "XmlFiller",
-    "any_to_pdf", "any_to_pdf_async", "UnsupportedFormat", "ConversionProgress",
-    "split_pdf", "merge_pdf"
+    # XLSX to XML
+    "XlsxToXmlConverter",
+    "XlsxReader",
+    "XmlWriter",
+    "XmlFiller",
+    # PDF Conversion
+    "any_to_pdf",
+    "any_to_pdf_async",
+    "UnsupportedFormat",
+    "ConversionProgress",
+    # PDF Splitter
+    "split_pdf",
+    "split_pdf_sequential",
+    "split_pdf_parallel",
+    # PDF Merger
+    "merge_pdf",
+    "merge_pdf_sequential",
+    "merge_pdf_parallel",
 ]
--- src/pdf/__init__.py
+++ src/pdf/__init__.py
@@ -1,13 +1,38 @@
 """
 PDF Converter Package
+
+Modules:
+    - any_to_pdf: Convert various file formats to PDF
+    - splitter: Split PDF files into multiple files
+    - merger: Merge multiple PDF files into one
 """
 
 from .any_to_pdf import (
-    any_to_pdf, any_to_pdf_async, UnsupportedFormat, ConversionProgress,
-    split_pdf, merge_pdf
+    any_to_pdf,
+    any_to_pdf_async,
+    UnsupportedFormat,
+    ConversionProgress,
+    excel_to_pdf_large_file,
+    excel_to_pdf_alternative,
 )
+from .splitter import split_pdf, split_pdf_sequential, split_pdf_parallel
+from .merger import merge_pdf, merge_pdf_sequential, merge_pdf_parallel
 
 __all__ = [
-    "any_to_pdf", "any_to_pdf_async", "UnsupportedFormat", "ConversionProgress",
-    "split_pdf", "merge_pdf"
+    # Conversion functions
+    "any_to_pdf",
+    "any_to_pdf_async",
+    "UnsupportedFormat",
+    "ConversionProgress",
+    # Splitter functions
+    "split_pdf",
+    "split_pdf_sequential",
+    "split_pdf_parallel",
+    # Merger functions
+    "merge_pdf",
+    "merge_pdf_sequential",
+    "merge_pdf_parallel",
+    # Excel helpers
+    "excel_to_pdf_large_file",
+    "excel_to_pdf_alternative",
 ]
